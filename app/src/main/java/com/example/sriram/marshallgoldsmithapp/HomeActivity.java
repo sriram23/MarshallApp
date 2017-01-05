e88ef72223322290bb3db895a4023d85345557bf
sriram23 <sriramsubramanian23@gmail.com>
2017-01-05 13:48:53 +0530

    hell

---
+++ app/src/main/java/com/example/sriram/marshallgoldsmithapp/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.sriram.marshallgoldsmithapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        System.out.println("hell");
+    }
+}
