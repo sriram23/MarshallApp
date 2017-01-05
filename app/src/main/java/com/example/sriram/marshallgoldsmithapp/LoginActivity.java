bddc50fa73829737fc616f380218e71e3f48b336
sriram23 <sriramsubramanian23@gmail.com>
2017-01-05 15:40:04 +0530

    First Commit of Marshall App

---
+++ app/src/main/java/com/example/sriram/marshallgoldsmithapp/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.sriram.marshallgoldsmithapp;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+// get editor to edit in file
+
+    }
+}
