face284b3a8a9c01e07302a777528843fdd1ffb5
sriram23 <sriramsubramanian23@gmail.com>
2017-01-07 16:20:34 +0530

    Actions given to Login and signup button of Home

--- app/src/main/java/com/example/sriram/marshallgoldsmithapp/HomeActivity.java
+++ app/src/main/java/com/example/sriram/marshallgoldsmithapp/HomeActivity.java
@@ -1,13 +1,32 @@
 package com.example.sriram.marshallgoldsmithapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
+    Button Login,Signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Login = (Button) findViewById(R.id.login_button);
+        Signup = (Button) findViewById(R.id.signup_button);
+        Login.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        Signup.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
