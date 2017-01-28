32f55f832635aea83e5916fc37a4a3d73fbd8fd1
sriram23 <sriramsubramanian23@gmail.com>
2017-01-28 16:06:04 +0530

    Added some menu

--- app/src/main/java/com/example/sriram/marshallgoldsmithapp/HomeActivity.java
+++ app/src/main/java/com/example/sriram/marshallgoldsmithapp/HomeActivity.java
@@ -1,12 +1,15 @@
 package com.example.sriram.marshallgoldsmithapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
     Button Login,Signup;
@@ -41,6 +44,30 @@ public class HomeActivity extends AppCompatActivity {
 
         return true;
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                Intent intent0 = new Intent(Intent.ACTION_VIEW, Uri.parse("http:\\mail.coderdojocoimbatore.site"));
+                startActivity(intent0);
+                return true;
+            case R.id.Signin_menu:
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.Signup_menu:
+                Intent intent1 = new Intent(HomeActivity.this, SignupActivity.class);
+                startActivity(intent1);
+                return true;
+            case R.id.Exit:
+                moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
--- app/src/main/java/com/example/sriram/marshallgoldsmithapp/LoginActivity.java
+++ app/src/main/java/com/example/sriram/marshallgoldsmithapp/LoginActivity.java
@@ -2,8 +2,12 @@ package com.example.sriram.marshallgoldsmithapp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,5 +25,36 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.menusec, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://mail.coderdojocoimbatore.site"));
+                startActivity(intent);
+                return true;
+            case R.id.Home:
+                Intent intent1 = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(intent1);
+                return true;
+            case R.id.Exit:
+                moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
